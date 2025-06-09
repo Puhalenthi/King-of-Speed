92d7e2d20f6f9cf9adaacdbf22f1da8ff16eb259
matousposp08 <154290575+matousposp08@users.noreply.github.com>
2025-06-09 19:45:37 -0400

    .

--- kingofspeed/Scenes/local_menu.gd
+++ kingofspeed/Scenes/local_menu.gd
@@ -1,4 +1,5 @@
 extends Node2D
+var button_type = null
 func _ready():
 	$Lfade/Ltimer.start()
 	$Lfade/AnimationPlayer.play("fadeout")
@@ -6,3 +7,15 @@ func _ready():
 
 func _on_ltimer_timeout() -> void:
 	$Lfade.hide()
+
+
+func _on_back_to_main_pressed() -> void:
+	button_type = "back1"
+	$LLfade.show()
+	$LLfade/LLtimer.start()
+	$LLfade/AnimationPlayer.play("fadein")
+
+
+func _on_l_ltimer_timeout() -> void:
+	if button_type == "back1":
+		get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
