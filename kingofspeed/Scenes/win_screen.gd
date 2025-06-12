f3a089fb085bd25716c2b3948479e136681eed66
dVillarosa287 <davidpaul.villarosa@gmail.com>
2025-06-12 09:12:24 -0400

    add music

--- kingofspeed/Scenes/win_screen.gd
+++ kingofspeed/Scenes/win_screen.gd
@@ -1,1 +1,9 @@
 extends CanvasLayer
+
+
+func _on_rematch_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_back_to_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
