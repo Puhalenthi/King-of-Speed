cf12912e5adc221c6d610764907afc845815f134
dVillarosa287 <davidpaul.villarosa@gmail.com>
2025-06-12 07:56:01 -0400

    Revert "Merge pull request #11 from Puhalenthi/feature/add-music"
    This reverts commit a3c20d36e1e3d06d662686194e7bcea573a18885, reversing
    changes made to c640dd4effbeedb366d4e1e6896f12457eb2a9f9.

--- kingofspeed/Scenes/win_screen.gd
+++ kingofspeed/Scenes/win_screen.gd
@@ -1,10 +1,1 @@
 extends CanvasLayer
-
-
-
-
-func _on_back_to_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/LocalMenu.tscn")
-
-func _on_rematch_pressed() -> void:
-	get_tree().reload_current_scene()
